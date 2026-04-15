311b850c6f9133344217c44d75e91b0d9a17ba15
aksworns22 <wkdeogks17@gmail.com>
2026-04-15 17:30:35 +0900

    test: 결제 흐름 전체에 대한 테스트 추가

--- src/test/kotlin/payment/DefaultMoviePaymentTest.kt
+++ src/test/kotlin/payment/DefaultMoviePaymentTest.kt
@@ -7,6 +7,7 @@ import model.movie.RunningTime
 import model.payment.DefaultMoviePayment
 import model.payment.Money
 import model.payment.PayType
+import model.payment.PayTypeDiscount
 import model.payment.Point
 import model.reservation.MovieReservationGroup
 import model.reservation.MovieSeatSelection
@@ -22,7 +23,7 @@ import java.time.LocalDateTime
 
 class DefaultMoviePaymentTest {
     @Test
-    fun `좌석 등급과 예약한 좌석 수에 따라서 가격이 계산된다`() {
+    fun `좌석 등급과 예약한 좌석 수에 따라서 총 가격이 계산된다`() {
         val reservations =
             MovieReservationGroup(
                 setOf(
@@ -51,5 +52,140 @@ class DefaultMoviePaymentTest {
             point = Point(0),
             payType = PayType.CREDIT_CARD,
         ).calculate().totalPrice shouldBe Money(30_000)
+    }
+
+    @Test
+    fun `4월 10일 13시 영화를 0포인트와 현금으로 구매하면 무비데이 할인과 현금 할인 적용된다`() {
+        // given
+        val movieSeatSelection =
+            MovieSeatSelection(
+                movie = Movie(MovieName("옥탑방에사는남자"), RunningTime(60)),
+                screenTime =
+                    CinemaTimeRange(
+                        start = CinemaTime(LocalDateTime.of(2026, 4, 10, 13, 30)),
+                        end = CinemaTime(LocalDateTime.of(2026, 4, 10, 14, 30)),
+                    ),
+                seat = Seat(SeatPosition(SeatRow("A"), SeatColumn(1)), SeatGrade.A),
+            )
+        val reservations =
+            MovieReservationGroup(
+                setOf(movieSeatSelection),
+            )
+        val finalPayAmount =
+            (movieSeatSelection.price applyRate 0.9) applyRate (1 - PayTypeDiscount.CASH_DISCOUNT_RATIO)
+
+        // when
+        val moviePaymentResult =
+            DefaultMoviePayment(
+                reservations = reservations,
+                point = Point(0),
+                payType = PayType.CASH,
+            ).calculate()
+
+        // then
+        moviePaymentResult.finalPrice shouldBe finalPayAmount
+    }
+
+    @Test
+    fun `4월 10일 7시 영화를 5000포인트와 현금으로 구매하면 무비데이 할인, 조조할인, 현금 할인 적용된다`() {
+        // given
+        val movieSeatSelection =
+            MovieSeatSelection(
+                movie = Movie(MovieName("옥탑방에사는남자"), RunningTime(60)),
+                screenTime =
+                    CinemaTimeRange(
+                        start = CinemaTime(LocalDateTime.of(2026, 4, 10, 7, 30)),
+                        end = CinemaTime(LocalDateTime.of(2026, 4, 10, 8, 30)),
+                    ),
+                seat = Seat(SeatPosition(SeatRow("A"), SeatColumn(1)), SeatGrade.A),
+            )
+        val reservations =
+            MovieReservationGroup(
+                setOf(movieSeatSelection),
+            )
+        val usedPoint = Point(5_000)
+        val appliedMovieDayDiscount = movieSeatSelection.price applyRate 0.9
+        val appliedEarlyMorningDiscount = appliedMovieDayDiscount - Money(2_000)
+        val finalPayAmount =
+            (appliedEarlyMorningDiscount - usedPoint.toMoney()) applyRate (1 - PayTypeDiscount.CASH_DISCOUNT_RATIO)
+
+        // when
+        val moviePaymentResult =
+            DefaultMoviePayment(
+                reservations = reservations,
+                point = usedPoint,
+                payType = PayType.CASH,
+            ).calculate()
+
+        // then
+        moviePaymentResult.finalPrice shouldBe finalPayAmount
+    }
+
+    @Test
+    fun `4월 10일 23시 영화를 0포인트와 신용카드로 구매하면 무비데이 할인, 심야할인, 카드 할인 적용된다`() {
+        // given
+        val movieSeatSelection =
+            MovieSeatSelection(
+                movie = Movie(MovieName("옥탑방에사는남자"), RunningTime(60)),
+                screenTime =
+                    CinemaTimeRange(
+                        start = CinemaTime(LocalDateTime.of(2026, 4, 10, 23, 0)),
+                        end = CinemaTime(LocalDateTime.of(2026, 4, 11, 0, 0)),
+                    ),
+                seat = Seat(SeatPosition(SeatRow("A"), SeatColumn(1)), SeatGrade.A),
+            )
+        val reservations =
+            MovieReservationGroup(
+                setOf(movieSeatSelection),
+            )
+        val appliedMovieDayDiscount = movieSeatSelection.price applyRate 0.9
+        val appliedLateNightDiscount = appliedMovieDayDiscount - Money(2_000)
+        val finalPayAmount =
+            (appliedLateNightDiscount - Money(0)) applyRate (1 - PayTypeDiscount.CREDIT_CARD_DISCOUNT_RATIO)
+
+        // when
+        val moviePaymentResult =
+            DefaultMoviePayment(
+                reservations = reservations,
+                point = Point(0),
+                payType = PayType.CREDIT_CARD,
+            ).calculate()
+
+        // then
+        moviePaymentResult.finalPrice shouldBe finalPayAmount
+    }
+
+    @Test
+    fun `4월 11일 23시 영화를 500포인트와 신용카드로 구매하면 심야할인과 카드 할인 적용된다`() {
+        // given
+        val movieSeatSelection =
+            MovieSeatSelection(
+                movie = Movie(MovieName("옥탑방에사는남자"), RunningTime(60)),
+                screenTime =
+                    CinemaTimeRange(
+                        start = CinemaTime(LocalDateTime.of(2026, 4, 11, 23, 0)),
+                        end = CinemaTime(LocalDateTime.of(2026, 4, 12, 0, 0)),
+                    ),
+                seat = Seat(SeatPosition(SeatRow("A"), SeatColumn(1)), SeatGrade.A),
+            )
+        val reservations =
+            MovieReservationGroup(
+                setOf(movieSeatSelection),
+            )
+        val usedPoint = Point(500)
+        val appliedLateNightDiscount = movieSeatSelection.price - Money(2_000)
+        val finalPayAmount =
+            (appliedLateNightDiscount - usedPoint.toMoney()) applyRate (1 - PayTypeDiscount.CREDIT_CARD_DISCOUNT_RATIO)
+
+        // when
+        val moviePaymentResult =
+            DefaultMoviePayment(
+                reservations = reservations,
+                point = usedPoint,
+                payType = PayType.CREDIT_CARD,
+            ).calculate()
+
+        // then
+        moviePaymentResult.finalPrice shouldBe finalPayAmount
     }
 }
